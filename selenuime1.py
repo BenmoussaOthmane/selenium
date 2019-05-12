dcb435897b204a378156c7d4dbce20a5aa5b02d0
BenmoussaOthmane <benmoussaothmane@iMac-de-BenmoussaOthmane.local>
2019-05-12 19:09:43 +0100

    *****

--- selenuime1.py
+++ selenuime1.py
@@ -7,6 +7,12 @@ driver = wb.Chrome('/Users/benmoussaothmane/Downloads/chromedriver')
 driver.get('https://www.amazon.fr/s?k=imac&__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss_2')
 
 source = driver.page_source
+# div = driver.find_elements_by_xpath("""//*[@class="a-section"]""").click()
 
-div  = driver.find_element_by_class_name('s-include-content-margin')
-print(len(div))
+price = driver.find_elements_by_class_name('a-price-whole')
+
+for p in price:
+    print(p.text)
+
+
+driver.close()
